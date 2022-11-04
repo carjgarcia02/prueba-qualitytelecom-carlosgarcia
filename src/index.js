5f08b510a6f0b5454ca4a038fe65492ea7a47fe6
carjgarcia02 <carjgarcia02@gmail.com>
2022-11-04 01:20:15 -0500

    Arreglos en validaciones y nombres de variables

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
 /* Para este ejemplo se decidió que el usuario ingresará con su email y contraseña.*/
-document.getElementById("formulario").addEventListener("submit", (e) => {
+document.getElementById("form").addEventListener("submit", (e) => {
   e.preventDefault();
 
   //Campos para ingresar los datos
-  let usuario = document.getElementById("usuario");
-  let contra = document.getElementById("contraseña");
+  let user = document.getElementById("user");
+  let pass = document.getElementById("password");
 
   //Mensaje de validación de datos
-  const mensaje = document.getElementById("message");
+  const msg = document.getElementById("message");
 
   //Expresiones regulares
-  const expRegUsuario = /^[^@\s]+@[^@\s]+\.[^@\s]+$/.test(usuario.value);
+  const regExpUser = /^[^@\s]+@[^@\s]+\.[^@\s]+$/.test(user.value);
 
-  const expRegContra =
+  const regExpPass =
     /^(?=(?:.*\d))(?=.*[A-Z])(?=.*[a-z])(?=.*[.,*!?¿¡/#$%&])\S{8,64}$/.test(
-      contra.value
+      pass.value
     ); //Por lo menos un dígito 0-9, una mayúscula, una minúscula y un caracter especial, no acepta espacios, longitud 8-64 caracteres.
 
   //Validaciones
-  if (usuario.value === "" || contra.value === "") {
+  /* if (usuario.value === "" || contra.value === "") {
     mensaje.classList.remove("bg-green-600");
     mensaje.classList.add("bg-red-600");
-    mensaje.innerHTML = "Hay campos vacíos, por favor llénelos todos.";
+    mensaje.textContent = "Hay campos vacíos, por favor llénelos todos.";
     if (usuario.value === "") {
       usuario.style.boxShadow = "0 0 5px red";
       usuario.focus();
@@ -44,5 +44,32 @@ document.getElementById("formulario").addEventListener("submit", (e) => {
     }
     mensaje.classList.add("bg-green-600");
     mensaje.innerHTML = "Datos correctos, puede ingresar al sistema.";
+  } */
+
+  if(pass.value === "" || pass.value === null){
+    msg.classList.add("bg-red-600");
+    msg.textContent = "Por favor escriba una contraseña.";
+    pass.style.boxShadow = "0 0 5px red";
+    pass.focus();
   }
+
+  if(user.value === "" || user.value === null){
+    msg.classList.add("bg-red-600");
+    msg.textContent = "Debe ingresar un nombre de usuario.";
+    user.style.boxShadow = "0 0 5px red";
+    pass.style.boxShadow = "none";
+    user.focus();
+  }
+
+  if (!regExpUser || !regExpPass) {
+    msg.classList.add("bg-red-600");
+    msg.textContent = "Por favor digite correo y contraseña válidos.";
+  }
+
+  if (regExpUser && regExpPass) {
+    msg.classList.remove("bg-red-600");
+    msg.classList.add("bg-green-600");
+    msg.textContent = "Datos correctos, puede ingresar al sistema.";
+  }
+
 });
